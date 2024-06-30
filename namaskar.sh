72db5c4818908303696e0d4f3a0fbbfa58b94a19
stuffbymax <96355058+stuffbymax@users.noreply.github.com>
2024-06-30 01:54:45 +0100

    Update namaskar.sh

--- namaskar.sh
+++ namaskar.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
+
+# Function to calculate percentage
+get_percentage() {
+    used=$1
+    total=$2
+    awk -v used="$used" -v total="$total" 'BEGIN { printf("%.2f%%", (used / total) * 100) }'
+}
+
+# Get memory and swap usage details
+memory_usage=$(free -h --si | awk '/^Mem/ {print $3 " / " $2}')
+swap_usage=$(free -h --si | awk '/^Swap/ {print $3 " / " $2}')
+
+# Calculate percentage for memory
+memory_used=$(free --si | awk '/^Mem/ {print $3}')
+memory_total=$(free --si | awk '/^Mem/ {print $2}')
+memory_percentage=$(get_percentage "$memory_used" "$memory_total")
+
+# Calculate percentage for swap
+swap_used=$(free --si | awk '/^Swap/ {print $3}')
+swap_total=$(free --si | awk '/^Swap/ {print $2}')
+swap_percentage=$(get_percentage "$swap_used" "$swap_total")
+
 host=$(uname -n)
 user=$(whoami)
 shell=$(echo $SHELL)
@@ -6,6 +28,8 @@ kernel="$(uname -r)"
 shell="$(basename "${SHELL}")"
 os="$(uname -s)"
 uptime="$(uptime -p)"
+
+    echo""
 c1=$(echo -e "\e[42m  \e[0m")
 c2=$(echo -e "\e[41m  \e[0m")
 c3=$(echo -e "\e[43m  \e[0m")
@@ -19,6 +43,8 @@ ${c2} │ ┌─┼─┐ host   : ${host}
 ${c3} │ │ │ │ user   : ${user}
 ${c4} └─┼─┘ │ kernel : ${kernel}
 ${c5}   └───┘ shell  : ${shell}
+${c5}   └───┘ ram    : $($memory_percentage) ($memory_usage
+${c5}   └───┘ swap    : $($swap_percentage) ($swap_usage)
 
   ${uptime}
 
